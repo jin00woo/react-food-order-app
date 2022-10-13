35194fcb9c7c13e5a0e7569eabb46484e09e0a1e
Jinwoo Han <56533546+jin00woo@users.noreply.github.com>
2022-10-13 14:06:59 +0000

    Added logic so that the cart component shows amount change when buttons are clicked.

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -30,7 +30,7 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(meal => <MealItem key={meal.id} name={meal.name} description={meal.description} price={meal.price}>{meal.name}</MealItem>);
+  const mealsList = DUMMY_MEALS.map(meal => <MealItem key={meal.id} id={meal.id} name={meal.name} description={meal.description} price={meal.price}>{meal.name}</MealItem>);
   
   return <section className={classes.meals}>
     <Card>
